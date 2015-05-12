56f999c120d614bb0d25c22d911132276debef21
fcarribero <32594073>
2015-05-11 21:37:51 -0300

    SMTP configuration is now being loaded from config.json file

--- application/config/mail.php
+++ application/config/mail.php
@@ -1,10 +1,18 @@
 <?php
 
 $config['protocol'] = "smtp";
-$config['smtp_host'] = "mail.webneex.com";
-$config['smtp_port'] = "25";
-$config['smtp_user'] = "francisco.carribero@webneex.com"; 
-$config['smtp_pass'] = "Mar3ng0$";
+$config['smtp_host'] = "";
+$config['smtp_port'] = "";
+$config['smtp_user'] = "";
+$config['smtp_pass'] = "";
 $config['charset'] = "utf-8";
 $config['mailtype'] = "html";
 $config['newline'] = "\r\n";
+
+if (($local_config = @json_decode(file_get_contents('config.json'), true))) {
+    $config['smtp_host'] = $local_config['mail']['smtp_host'];
+    $config['smtp_port'] = $local_config['mail']['smtp_port'];
+    $config['smtp_user'] = $local_config['mail']['smtp_user'];
+    $config['smtp_pass'] = $local_config['mail']['smtp_pass'];
+}
+
